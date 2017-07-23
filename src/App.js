3d34608b2513b30748298253b50d3c11f2086a13
shmam <samuel.d.crochet@gmail.com>
2017-07-23 13:52:24 -0400

    goodbye azure, hello firebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
 import Create from './Create.js'
 import Post from './Post.js'
+import * as firebase from 'firebase';
+import 'whatwg-fetch'
 import './App.css';
 
-const colors = ['#ffb3b3','#9fbedf','#b3b3cc','#9fdf9f','#ffcc80','#cccc99','#9cb9ff','#6a8e81','#8e5b5b','#c8c1a5','#c6b5a7','#adc0b4','	#9db1bd','#bda5bc',	'#a19cb5','#f7a889','#f9d0a4','#f0debc','#f2e9af','#a4b28f','#786a94','#6c6389'];
+const config = require('./config/config.js');
+firebase.initializeApp(config);
+
+navigator.geolocation.getCurrentPosition(function(position) {
+  console.log(position)
+});
+
+//const colors = ['#ffb3b3','#9fbedf','#b3b3cc','#9fdf9f','#ffcc80','#cccc99','#9cb9ff','#6a8e81','#8e5b5b','#c8c1a5','#c6b5a7','#adc0b4','	#9db1bd','#bda5bc',	'#a19cb5','#f7a889','#f9d0a4','#f0debc','#f2e9af','#a4b28f','#786a94','#6c6389'];
+const db = firebase.database();
 
 
 class App extends Component {
+  
+  
+  constructor(props){
+    super(props);
+   
+    this.state = {
+      quote: "{Loading...}", 
+    }
 
+    this.componentDidMount = this.componentDidMount.bind(this);
+    
+  }
+
+
+
+  componentDidMount(){
+    //The .bind(then) was necessary to solve some bug where setState was not defined 
+    //This is called when 
+    
+    const dbref = db.ref('/quotes')
+    dbref.on('value', snap => {
+      var quoteArr = snap.val()
+      var num = Math.floor(Math.random() * (quoteArr.length));
+      console.log(num)
+      this.setState({quote:quoteArr[num]}) 
+    })
+  }
 
   render() {
-    const quote = "To be or not to be that is the beloved question that is being asked, but I can find no answer"
-    return (
+    
+    this.componentDidMount = this.componentDidMount.bind(this);
+    return(
       <div className = "App">
         <Title />  
-        <Controller quote={quote}/>
+        <Controller quote={this.state.quote}/>
       </div>
     );
   }
@@ -36,7 +73,8 @@ class Controller extends Component{
   constructor(props){
     super(props)
     this.state = {
-      posts: []
+      posts: [],
+      controller: "create"
     }
   }
 
@@ -52,20 +90,39 @@ class Controller extends Component{
     
   }
 
+  panelControl(){
 
-  render(){
     var renderPost = this.renderPost.bind(this);
     var renderCreate = this.renderCreate.bind(this);
     var foo = this.state.posts;
-    return(
-      <div className="Controller">
-        <button> create. </button>
-        <button> explore. </button>
+    
+    if(this.state.controller === "create"){
+      return(
+        <div className="Controller">
+        <button onClick={() => this.setState({controller: "create"})}> create. </button>
+        <button onClick={() => this.setState({controller: "explore"})}> explore. </button>
         <Create words={this.props.quote} post={renderPost} reload={renderCreate}/>
         {foo.reverse().map(function(post,i){
-          return(<Post value={post} key={i} date={(new Date())}/>)
+          return(<Post value={post} key={i} />)
         })}
       </div>
+      );
+    }
+    else if(this.state.controller === "explore"){
+      return(
+        <div className="Controller">
+        <button onClick={() => this.setState({controller: "create"})}> create. </button>
+        <button onClick={() => this.setState({controller: "explore"})}> explore. </button>
+        <p> explore posts </p>
+        </div>
+      );
+    }
+  }
+
+  render(){
+
+    return(
+      this.panelControl()
     );
   }
 
--- src/Post.js
+++ src/Post.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const colors = ['#ffb3b3','#9fbedf','#b3b3cc','#9fdf9f','#ffcc80','#cccc99','#9cb9ff','#6a8e81','#8e5b5b','#c8c1a5','#c6b5a7','#adc0b4','	#9db1bd','#bda5bc',	'#a19cb5','#f7a889','#f9d0a4','#f0debc','#f2e9af','#a4b28f','#786a94','#6c6389'];
+//const colors = ['#ffb3b3','#9fbedf','#b3b3cc','#9fdf9f','#ffcc80','#cccc99','#9cb9ff','#6a8e81','#8e5b5b','#c8c1a5','#c6b5a7','#adc0b4','	#9db1bd','#bda5bc',	'#a19cb5','#f7a889','#f9d0a4','#f0debc','#f2e9af','#a4b28f','#786a94','#6c6389'];
 
 class Post extends Component{
     
     render(){
-        const num = Math.floor(Math.random() * ((colors.length-1) + 1)) + 0;
-        const style = {
-            backgroundColor: colors[num],
-        };
+
 
         var twittervar = this.props.value
+        // eslint-disable-next-line
         twittervar = twittervar.replace(/[.,\/#!$\^&\*;:{}=\-_`~()]/g,"");
         twittervar = twittervar.split(" ")
         twittervar = twittervar.join("%20")
         
         console.log(twittervar)
         return(
-            <div className="Post" style={style}>
+            <div className="Post" >
                
-                <p className="content">{"'" + this.props.value + "'"}</p>
+                <p className="content">{this.props.value}</p>
                  <p className="username">{this.props.date}</p>
                 <a className="twitter-share-button"  href={"https://twitter.com/intent/tweet?text=" + twittervar}> tweet. </a> 
            </div>
